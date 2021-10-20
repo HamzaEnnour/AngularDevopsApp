b8df94347ee5bb3733e0ccb516686b775f163aef
HamzaEnnour <49129326+HamzaEnnour@users.noreply.github.com>
2021-10-20 13:35:41 +0100

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  SCARF_ANALYTICS: false,
+  apiUrl: '',
+  login: "SYS_LOG",
+  remember: "SYS_SEC",
+  isAuthGuardActive: true,
+  defaultMenuType: 'menu-default',
+  subHiddenBreakpoint: 1440,
+  menuHiddenBreakpoint: 768,
+};
